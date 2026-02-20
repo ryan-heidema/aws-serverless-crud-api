90d73b7d98c995f4e8f5bc426585ef7172fe0096
ryan-heidema <ryanheidema39@gmail.com>
2026-02-19 21:42:24 -0500

    add aws infra

--- infra/lib/infra-stack.ts
+++ infra/lib/infra-stack.ts
@@ -1,8 +1,120 @@
-import * as cdk from 'aws-cdk-lib/core';
-import { Construct } from 'constructs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
+import * as apigatewayv2 from "aws-cdk-lib/aws-apigatewayv2";
+import * as integrations from "aws-cdk-lib/aws-apigatewayv2-integrations";
+import * as path from "path";
 
 export class InfraStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    // DynamoDB Table
+    const table = new dynamodb.Table(this, "ItemsTable", {
+      partitionKey: { name: "id", type: dynamodb.AttributeType.STRING },
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+      removalPolicy: cdk.RemovalPolicy.DESTROY, // For dev - change to RETAIN for prod
+    });
+
+    // Lambda function for creating items
+    const createFunction = new NodejsFunction(this, "CreateFunction", {
+      runtime: lambda.Runtime.NODEJS_20_X,
+      handler: "handler",
+      entry: path.join(__dirname, "../../src/handlers/create.ts"),
+      environment: {
+        TABLE_NAME: table.tableName,
+      },
+    });
+
+    // Lambda function for getting items
+    const getFunction = new NodejsFunction(this, "GetFunction", {
+      runtime: lambda.Runtime.NODEJS_20_X,
+      handler: "handler",
+      entry: path.join(__dirname, "../../src/handlers/get.ts"),
+      environment: {
+        TABLE_NAME: table.tableName,
+      },
+    });
+
+    // Lambda function for updating items
+    const updateFunction = new NodejsFunction(this, "UpdateFunction", {
+      runtime: lambda.Runtime.NODEJS_20_X,
+      handler: "handler",
+      entry: path.join(__dirname, "../../src/handlers/update.ts"),
+      environment: {
+        TABLE_NAME: table.tableName,
+      },
+    });
+
+    // Lambda function for deleting items
+    const deleteFunction = new NodejsFunction(this, "DeleteFunction", {
+      runtime: lambda.Runtime.NODEJS_20_X,
+      handler: "handler",
+      entry: path.join(__dirname, "../../src/handlers/delete.ts"),
+      environment: {
+        TABLE_NAME: table.tableName,
+      },
+    });
+
+    // Grant Lambda functions access to DynamoDB table
+    table.grantReadWriteData(createFunction);
+    table.grantReadWriteData(getFunction);
+    table.grantReadWriteData(updateFunction);
+    table.grantReadWriteData(deleteFunction);
+
+    // API Gateway HTTP API
+    const httpApi = new apigatewayv2.HttpApi(this, "HttpApi", {
+      description: "CRUD API for Items",
+    });
+
+    // Add routes
+    httpApi.addRoutes({
+      path: "/items",
+      methods: [apigatewayv2.HttpMethod.POST],
+      integration: new integrations.HttpLambdaIntegration(
+        "CreateIntegration",
+        createFunction
+      ),
+    });
+
+    httpApi.addRoutes({
+      path: "/items/{id}",
+      methods: [apigatewayv2.HttpMethod.GET],
+      integration: new integrations.HttpLambdaIntegration(
+        "GetIntegration",
+        getFunction
+      ),
+    });
+
+    httpApi.addRoutes({
+      path: "/items/{id}",
+      methods: [apigatewayv2.HttpMethod.PUT],
+      integration: new integrations.HttpLambdaIntegration(
+        "UpdateIntegration",
+        updateFunction
+      ),
+    });
+
+    httpApi.addRoutes({
+      path: "/items/{id}",
+      methods: [apigatewayv2.HttpMethod.DELETE],
+      integration: new integrations.HttpLambdaIntegration(
+        "DeleteIntegration",
+        deleteFunction
+      ),
+    });
+
+    // Outputs
+    new cdk.CfnOutput(this, "ApiUrl", {
+      value: httpApi.url!,
+      description: "HTTP API endpoint URL",
+    });
+
+    new cdk.CfnOutput(this, "TableName", {
+      value: table.tableName,
+      description: "DynamoDB table name",
+    });
   }
 }
--- infra/test/infra.test.ts
+++ infra/test/infra.test.ts
@@ -1,3 +1,115 @@
-test('stack test', () => {
-  // add tests here
+import { Template } from "aws-cdk-lib/assertions";
+import * as cdk from "aws-cdk-lib";
+import { InfraStack } from "../lib/infra-stack";
+
+describe("InfraStack", () => {
+  let app: cdk.App;
+  let stack: InfraStack;
+  let template: Template;
+
+  beforeEach(() => {
+    app = new cdk.App();
+    stack = new InfraStack(app, "TestStack", {
+      env: {
+        account: "123456789012",
+        region: "us-east-1",
+      },
+    });
+    template = Template.fromStack(stack);
+  });
+
+  it("creates a DynamoDB table", () => {
+    template.hasResourceProperties("AWS::DynamoDB::Table", {
+      BillingMode: "PAY_PER_REQUEST",
+      KeySchema: [
+        {
+          AttributeName: "id",
+          KeyType: "HASH",
+        },
+      ],
+      AttributeDefinitions: [
+        {
+          AttributeName: "id",
+          AttributeType: "S",
+        },
+      ],
+    });
+  });
+
+  it("creates four Lambda functions", () => {
+    template.resourceCountIs("AWS::Lambda::Function", 4);
+
+    // Check that all functions have the TABLE_NAME environment variable and correct runtime
+    template.hasResourceProperties("AWS::Lambda::Function", {
+      Runtime: "nodejs20.x",
+      Handler: "index.handler",
+    });
+
+    // Verify TABLE_NAME is set (it's a Ref to the table)
+    const functions = template.findResources("AWS::Lambda::Function");
+    Object.values(functions).forEach((func: any) => {
+      expect(func.Properties.Environment.Variables.TABLE_NAME).toBeDefined();
+    });
+  });
+
+  it("creates an API Gateway HTTP API", () => {
+    template.hasResourceProperties("AWS::ApiGatewayV2::Api", {
+      ProtocolType: "HTTP",
+      Description: "CRUD API for Items",
+    });
+  });
+
+  it("creates API routes for all CRUD operations", () => {
+    // Check for POST /items
+    template.hasResourceProperties("AWS::ApiGatewayV2::Route", {
+      RouteKey: "POST /items",
+    });
+
+    // Check for GET /items/{id}
+    template.hasResourceProperties("AWS::ApiGatewayV2::Route", {
+      RouteKey: "GET /items/{id}",
+    });
+
+    // Check for PUT /items/{id}
+    template.hasResourceProperties("AWS::ApiGatewayV2::Route", {
+      RouteKey: "PUT /items/{id}",
+    });
+
+    // Check for DELETE /items/{id}
+    template.hasResourceProperties("AWS::ApiGatewayV2::Route", {
+      RouteKey: "DELETE /items/{id}",
+    });
+  });
+
+  it("grants Lambda functions access to DynamoDB table", () => {
+    // Each Lambda function should have a policy allowing DynamoDB access
+    template.resourceCountIs("AWS::IAM::Policy", 4);
+
+    // Verify at least one policy has DynamoDB permissions
+    const policies = template.findResources("AWS::IAM::Policy");
+    const hasDynamoAccess = Object.values(policies).some((policy: any) => {
+      const statements = policy.Properties.PolicyDocument.Statement;
+      return statements.some((stmt: any) => {
+        return (
+          stmt.Effect === "Allow" &&
+          Array.isArray(stmt.Action) &&
+          stmt.Action.includes("dynamodb:PutItem") &&
+          stmt.Action.includes("dynamodb:GetItem") &&
+          stmt.Action.includes("dynamodb:UpdateItem") &&
+          stmt.Action.includes("dynamodb:DeleteItem")
+        );
+      });
+    });
+    expect(hasDynamoAccess).toBe(true);
+  });
+
+  it("outputs API URL and table name", () => {
+    template.hasOutput("ApiUrl", {
+      Description: "HTTP API endpoint URL",
+    });
+
+    template.hasOutput("TableName", {
+      Description: "DynamoDB table name",
+    });
+  });
 });
